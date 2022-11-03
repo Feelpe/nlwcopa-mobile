bef631277e90f4b74b91e8b01240ea6c50c4c4bb
felipe <felipesilvasantoslawliet1@gmail.com>
2022-11-03 16:22:20 -0300

    feat: add all components

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
 
 import { THEME } from './src/styles/theme';
 
@@ -20,7 +20,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <New /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
